8f62954da27ca4213f951227e093be82cea85312
Agus Gaspari <agusgaspari05@gmail.com>
2022-04-30 18:43:18 -0300

    modificación codigo .js con adicion de metodo filter

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Producto{
         let productoIngresado
         let continua = true;
         while (continua){
-            productoIngresado= parseInt(prompt("Ingrese producto que desea comprar: \n1 Bandolera \n2 Riñonera \n3 Bolsotote \n4 Tabaquera"));
+            productoIngresado= parseInt(prompt("Ingrese producto que desea comprar: \n1 Bandolera \n2 Riñonera \n3 Bolsotote \n4 Tabaquera \n5 Billetera grande \n6 Billetera chica"));
             if(productoIngresado==1){
                 this.nombre= "Bandolera"
                 alert(this.nombre.toUpperCase())
@@ -35,16 +35,24 @@ class Producto{
                 this.nombre= "tabaquera"
                 alert(this.nombre.toUpperCase())
                 continua = false;
+            } else if (productoIngresado==5) {
+                this.nombre= "billetera grande"
+                alert(this.nombre.toUpperCase())
+                continua = false;
+            } else if (productoIngresado==6) {
+                this.nombre= "billetera chica"
+                alert(this.nombre.toUpperCase())
+                continua = false;
             }
             else if (productoIngresado=="") {
                 alert("Ingrese un producto válido")
                 
             }
             else if (productoIngresado!=Number){
-                alert("Por favor ingrese una opción del 1 al 4");
+                alert("Por favor ingrese una opción del 1 al 6");
                 
-            } else if (productoIngresado>4 || productoIngresado<1){
-                alert("Por favor ingrese una opción del 1 al 4");
+            } else if (productoIngresado>6 || productoIngresado<1){
+                alert("Por favor ingrese una opción del 1 al 6");
                 
             }
             else{
@@ -59,91 +67,35 @@ class Producto{
             continua2=false;
             } else {
                 alert("Por favor ingrese un número válido. Hasta 10 unidades");  
-            } 
+            }
+            
         }
 
-        this.precio=Producto.precio*IVA*cantidadProductos;
-        console.log(Producto.this.precio)
-        
-        alert ("Monto a abonar = $ "+ this.precio)
+        let total=this.precio*IVA*cantidadProductos;
+        console.log(total)
+           
+        alert ("Monto a abonar = $ "+ total)
     }
     
 }
 const producto1 =new Producto("Bandolera",2000,30);
 const producto2=new Producto("Riñonera",1500,20);
-const producto3 =new Producto("Bolsotote",3000,15);
-const producto4 =new Producto("Tabaquera",1000,15);
+const producto3 =new Producto("Bolsotote",3500,15);
+const producto4 =new Producto("Tabaquera",1500,15);
+const producto5=new Producto ("Billetera Grande", 1000,10)
+const producto6=new Producto("Billetera Chica", 900, 10)
 
-let productos= [producto1,producto2,producto3,producto4]
+let productos= [producto1,producto2,producto3,producto4, producto5, producto6]
 
 productos.forEach ((productoEnArray,indice)=>{
     console.log(productoEnArray)
     console.log(indice)
 })
 
-producto1.precioFinal();
-producto2.precioFinal();
-producto3.precioFinal();
-producto4.precioFinal();
-
-/*let continua = true;
-
-while (continua){
-    let productoIngresado= parseInt(prompt("Ingrese producto que desea comprar: \n1 Bandolera \n2 Riñonera \n3 Bolsotote \n4 Tabaquera"));
-    if(productoIngresado==1){
-        this.nombre= "Bandolera"
-        alert(this.nombre.toUpperCase())
-        continua = false;
-    }
-    else if (productoIngresado==2) {
-        this.nombre= "riñonera"
-        alert(this.nombre.toUpperCase())
-        continua = false;
-    }
-    else if (productoIngresado==3) {
-        this.nombre= "bolsotote"
-        alert(this.nombre.toUpperCase())
-        continua = false;
-    }
-    else if (productoIngresado==4) {
-        this.nombre= "tabaquera"
-        alert(this.nombre.toUpperCase())
-        continua = false;
-    }
-    else if (productoIngresado=="") {
-        alert("Ingrese un producto válido")
-        
-    }
-    else if (productoIngresado!=Number){
-        alert("Por favor ingrese una opción del 1 al 4");
-        
-    } else if (productoIngresado>4 || productoIngresado<1){
-        alert("Por favor ingrese una opción del 1 al 4");
-        
-    }
-    else{
-        alert("Por favor ingrese un producto válido");
-    }
-}
-
-/*let continua2=true;
-
-while (continua2){
-    cantidadProductos= parseInt(prompt ("Ingrese el número de unidades a comprar"))
-    if(!(isNaN(cantidadProductos)) && !(cantidadProductos > 11)) {
-        console.log(cantidadProductos)
-        continua2=false;
-    }
-    else {
-        alert("Por favor ingrese un número válido. Hasta 10 unidades");  
-    } 
-}
+console.log(productos.filter(Producto =>Producto.precio < 2000));
+console.log(productos.filter(Producto =>Producto.nombre == "Bolsotote"));
 
 producto1.precioFinal();
 producto2.precioFinal();
 producto3.precioFinal();
 producto4.precioFinal();
-
-/*let total = Producto.precioFinal
-console.log(total)
-alert ("Monto a abonar = $ "+ total)*/
