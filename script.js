4197fc5c5a163bd35a52ff4a86b060e06bd6044b
Agus Gaspari <agusgaspari05@gmail.com>
2022-05-02 22:41:12 -0300

    combinacion JS  con html y css

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //PRIMER ENTREGA PROYECTO:
+//variable global
 const IVA = 1.21
 
-
+//clase
 class Producto{
     constructor(nombre,precio,stock){
         this.nombre=nombre;
@@ -9,13 +10,16 @@ class Producto{
         this.stock= stock;
         
     }
-
+    //Método
     precioFinal(){
+        //variables locales
         let cantidadProductos
         let productoIngresado
         let continua = true;
+        //ciclo
         while (continua){
             productoIngresado= parseInt(prompt("Ingrese producto que desea comprar: \n1 Bandolera \n2 Riñonera \n3 Bolsotote \n4 Tabaquera \n5 Billetera grande \n6 Billetera chica"));
+            //condicional
             if(productoIngresado==1){
                 this.nombre= "Bandolera"
                 alert(this.nombre.toUpperCase())
@@ -67,17 +71,19 @@ class Producto{
             continua2=false;
             } else {
                 alert("Por favor ingrese un número válido. Hasta 10 unidades");  
+                continua2=false;
             }
             
         }
 
         let total=this.precio*IVA*cantidadProductos;
         console.log(total)
-           
         alert ("Monto a abonar = $ "+ total)
-    }
-    
+        
+    }  
 }
+
+//Agregado de productos
 const producto1 =new Producto("Bandolera",2000,30);
 const producto2=new Producto("Riñonera",1500,20);
 const producto3 =new Producto("Bolsotote",3500,15);
@@ -85,16 +91,20 @@ const producto4 =new Producto("Tabaquera",1500,15);
 const producto5=new Producto ("Billetera Grande", 1000,10)
 const producto6=new Producto("Billetera Chica", 900, 10)
 
+//Declaro array
 let productos= [producto1,producto2,producto3,producto4, producto5, producto6]
 
+//Método de búsqueda sobre array
 productos.forEach ((productoEnArray,indice)=>{
     console.log(productoEnArray)
     console.log(indice)
 })
 
+//Método de filtrado sobre array
 console.log(productos.filter(Producto =>Producto.precio < 2000));
 console.log(productos.filter(Producto =>Producto.nombre == "Bolsotote"));
 
+//llamar funciones
 producto1.precioFinal();
 producto2.precioFinal();
 producto3.precioFinal();
